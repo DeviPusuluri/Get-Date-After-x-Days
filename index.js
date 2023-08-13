eee8b4e7275d152dffe7dad5374e7766663fc38c
DeviPusuluri <124484973+DeviPusuluri@users.noreply.github.com>
2023-08-13 18:10:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const addDays = require("date-fns/addDays");
 const getdaysafterxdays = (days) => {
   const date = addDays(new Date(2020,7,22), days);
-  //   return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
   return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 };
 
